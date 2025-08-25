b40ca71536b913dcba0540f6b703e6219ca00b04
Caleb Twigg <calebtwigg248@gmail.com>
2025-08-25 17:20:57 -0400

    Added some minor elements to Exercise One code

--- Homework 1/ExerciseOne.c
+++ Homework 1/ExerciseOne.c
@@ -5,16 +5,26 @@
 int main() {
 
     // Create the results csv file
+    FILE *fp = fopen("results.csv", "w");
 
     // Flush the cache
+    _mm_clflush();
 
-    // Start the loop
-
+    // Loop 1M times
+    for (int i = 0; i < 1000000; i++) {
         // Time a cache miss
-        // Time a cache hit
-        // Store results in csv file
-        // Flush the cache
 
-    // Plot the results and store file
+        // Time a cache hit
+
+        // Store results in csv file
+
+        // Flush the cache
+        _mm_clflush();
+
+    }
+
+    // Close the results csv file pointer
+    fclose(fp);
+
     return 0;
 }
