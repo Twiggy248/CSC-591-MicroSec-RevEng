bfaa2a70b54e51b5f40f9826cac704054a058707
Caleb Twigg <calebtwigg248@gmail.com>
2025-08-25 19:55:12 -0400

    Updated Exercise One documentation

--- Homework 1/ExerciseOne.c
+++ Homework 1/ExerciseOne.c
@@ -2,25 +2,28 @@
 #include <emmintrin.h> // Library for _mm_clflush and _mm_mfence
 #include <immintrin.h> // Library for _rdtscp
 #include <string.h>
-
-// Exercise One: Time a cache hit and miss, report the results. 
-// Repeat 1M times and plot a distribution of data collected for misses and hits.
+/**
+    @file ExerciseOne.c
+    @author Caleb Twigg
+    Exercise One: Time a cache hit and miss and report the results.
+    Repeat 1M times and plot a distribution of data collected for misses and hits.
+*/
 int main() {
 
     // Create the results csv file
     FILE *fp = fopen("Exercise1_Results.csv", "w");
     fprintf(fp, "Trail,Hit,Miss\n");
 
+    // Initialize variables for timing, temporary storage, and the test data loaded into cache
     int hitStart, hitEnd, missStart, missEnd, hitTime, missTime;
     int aux;
-
     volatile char __attribute__ (( aligned (4096) ) ) x = 'A';
 
     // Flush the cache
     _mm_clflush((const void*)&x);
     _mm_mfence();
 
-    // Loop 1M times
+    // Loop 1M times and record the results in the csv file
     for (int i = 0; i < 1000000; i++) {
         // Time a cache miss
         _mm_mfence();
