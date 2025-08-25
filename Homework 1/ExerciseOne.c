16d5260ca0d7a826cf3eb646d6f42ecef81805cc
Caleb Twigg <calebtwigg248@gmail.com>
2025-08-25 16:54:38 -0400

    Added outline to code for Exercise 1

--- Homework 1/ExerciseOne.c
+++ Homework 1/ExerciseOne.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+// Exercise One: Time a cache hit and miss, report the results. 
+// Repeat 1M times and plot a distribution of data collected for misses and hits.
 int main() {
-    printf("Hello, World!\n");
+
+    // Create the results csv file
+
+    // Flush the cache
+
+    // Start the loop
+
+        // Time a cache miss
+        // Time a cache hit
+        // Store results in csv file
+        // Flush the cache
+
+    // Plot the results and store file
     return 0;
 }
