3a3ba4eb0ca6bedd9fb928fa6e5f73883813778f
Caleb Twigg <calebtwigg248@gmail.com>
2025-08-27 10:21:54 -0400

    Updated Exercise Two code to only time the operation

--- Homework 1/ExerciseTwo.c
+++ Homework 1/ExerciseTwo.c
@@ -8,16 +8,18 @@
 */
 int main() {
 
-    // Initialize variables for timing, temporary storage, and the test data loaded into cache
+    // Initialize variables for timing and timing
     int beforeOp, afterOp, elapsedTime;
     int aux;
     
-    beforeOp = __rdtscp(&aux);
+    // Load in the virtual registers
     __m256 evens = _mm256_set_ps (2.0 , 4.0 , 6.0 , 8.0 , 10.0 , 12.0 , 14.0 , 16.0) ;
     __m256 odds = _mm256_set_ps (1.0 , 3.0 , 5.0 , 7.0 , 9.0 , 11.0 , 13.0 , 15.0) ;
     volatile __m256 result;
-    result = _mm256_mul_ps(evens,odds);
     
+    // Perform and time the operation
+    beforeOp = __rdtscp(&aux);
+    result = _mm256_mul_ps(evens,odds);
     afterOp = __rdtscp(&aux);
 
     printf("Time: %d\n", afterOp-beforeOp);
