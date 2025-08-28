8bd2226042b7c3127c09f90dabf329d0ce586e71
Caleb Twigg <calebtwigg248@gmail.com>
2025-08-28 11:07:59 -0400

    Made ExerciseFour code and Script

--- Homework 1/ExerciseFour.c
+++ Homework 1/ExerciseFour.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <immintrin.h> // Library for _rdtscp and AVX operations
+#include <immintrin.h> // Library for _rdtscp
 #include <string.h>
 #include <unistd.h> // Library for usleep
 
 /**
-    @file ExerciseFive.c
+    @file ExerciseFour.c
     @author Caleb Twigg
-    Exercise Five: Time an AVX multiplication, wait for 100 ms, and time another AVX multiplication. 
-    Is there a timing difference? Repeat 1M times and plot a distribution of data.
+    Exercise Four: Time an AVX512 multiplication, wait for 100 ms (for example with usleep), and time another AVX512 multiplication. Is
+    there a timing difference? Repeat 1M times and plot a distribution of data.
 */
 int main() {
 
@@ -15,24 +15,24 @@ int main() {
     // it was later revealed that it was information needed
 
     // Create the results csv file
-    FILE *fp = fopen("Exercise5_Results.csv", "w");
+    FILE *fp = fopen("Exercise4_Results.csv", "w");
     fprintf(fp, "Trial,First,Second,Difference\n");
 
     // Initialize variables for timing
     int beforeOp, afterOp, first, second;
     int aux;
     
-    // Load in the virtual registers
-    __m256 evens = _mm256_set_ps (2.0 , 4.0 , 6.0 , 8.0 , 10.0 , 12.0 , 14.0 , 16.0) ;
-    __m256 odds = _mm256_set_ps (1.0 , 3.0 , 5.0 , 7.0 , 9.0 , 11.0 , 13.0 , 15.0) ;
-    volatile __m256 result;
+    // Load in the virtual registers (this time using the mm512 operation in the AVX512 family)
+    __m512 evens = _mm512_set_ps (2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0, 22.0, 24.0, 26.0, 28.0, 30.0, 32.0);
+    __m512 odds = _mm512_set_ps (1.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0, 17.0, 19.0, 21.0, 23.0, 25.0, 27.0, 29.0, 31.0);
+    volatile __m512 result;
 
     // Loop 1M times and record the results in the csv file
     for (int i = 0; i < 1000000; i++) {
         
         // Perform and time the 1st operation
         beforeOp = __rdtscp(&aux);
-        result = _mm256_mul_ps(evens,odds);
+        result = _mm512_mul_ps(evens,odds);
         afterOp = __rdtscp(&aux);
 
         first = afterOp - beforeOp;
@@ -42,7 +42,7 @@ int main() {
 
         // Perform and time the 2nd operation
         beforeOp = __rdtscp(&aux);
-        result = _mm256_mul_ps(evens,odds);
+        result = _mm512_mul_ps(evens,odds);
         afterOp = __rdtscp(&aux);
 
         second = afterOp - beforeOp;
---
+++ Homework 1/Scripts/eFour.sc
@@ -0,0 +1,4 @@
+#! /bin/bash
+gcc -O0 -mavx512f ../ExerciseFour.c -o ExerciseFour
+
+./ExerciseFour
